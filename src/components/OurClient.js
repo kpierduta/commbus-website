37faf84acf0ff7bbe11c74a98dbac345c4156154
kunal <103kunalsharma@gmail.com>
2019-10-21 00:35:31 -0700

    minor fix

--- src/components/OurClient.js
+++ src/components/OurClient.js
@@ -8,50 +8,50 @@ const OurClient = () => (
     <div className="container">
       <h2 className="subtitle is-5 has-text-weight-bold">Trusted by</h2>
       <div className="columns is-multiline">
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           <img src="/images/brands/heinz-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/sainsburys-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/kier-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/sainsburys-client-logo-copy-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/staffordshire-university-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/breast-cancer-care-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/asda-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/gemalto-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/anglia-ruskin-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/fca-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/apprenticeships-client-logo-200x83.png" />
         </div>
-        <div className="column is-2">
+        <div className="column is-2 has-text-centered">
           {' '}
           <img src="/images/brands/uk-fast-client-logo-200x83.png" />
         </div>
