cd19a15c0cf35d576263a99177345f98b94899ea
iamkunall <103kunalsharma@gmail.com>
2019-12-16 21:56:46 +0530

    clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
-import FooterHeading from './FooterHeading';
 
 const Container = styled.section`
   font-family: ${props => props.theme.primaryFontFamily};
