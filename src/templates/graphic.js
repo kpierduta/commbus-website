94bf3499a7e24ac3302446076fd2087509070726
iamkunall <103kunalsharma@gmail.com>
2020-03-24 13:57:04 +0530

    features section added to vehicle blogs

--- src/templates/graphic.js
+++ src/templates/graphic.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 import HomeHero from '../components/HomeHero';
+import Features from '../components/Features';
 import Banner from '../components/Banner';
 import MessageInfo from '../components/MessageInfo';
 
@@ -34,6 +35,39 @@ export const pageQuery = graphql`
           url
         }
       }
+      featureSectionTitle
+      iconOne {
+        title
+        file {
+          url
+        }
+      }
+      featureOneTitle
+      featureOneSubtitle
+      iconTwo {
+        title
+        file {
+          url
+        }
+      }
+      featureTwoTitle
+      featureTwoSubtitle
+      iconThird {
+        title
+        file {
+          url
+        }
+      }
+      featureThirdTitle
+      featureThirdSubtitle
+      iconFour {
+        title
+        file {
+          url
+        }
+      }
+      featureFourTitle
+      featureFourSubtitle
       bannerTitle
       bannerImages {
         file {
@@ -68,6 +102,7 @@ export default class Graphic extends React.Component {
           mobileImage={page.mobileHeroImage.file.url}
           text="  "
         />
+        <Features Feature={page} />
         <Banner data={page} />
         <MessageInfo data={page} />
       </Layout>
