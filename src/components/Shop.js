249faa450f861fbb2c999946b54aa61dbc00a921
dharmveer97 <dvbangar8@gmail.com>
2020-06-16 11:40:58 +0530

    fix button

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import styled from 'styled-components';
 import ImageModal from './ImageModal';
-import Button from './elements/Button';
 
 const Section = styled.div`
   .has-text-info {
     margin-bottom: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  width: 14rem;
+  background: transparent;
+  color: ${props => props.theme.mainBrandColor};
+  border: 1px solid ${props => props.theme.mainBrandColor};
+  :hover {
+    color: ${props => props.theme.mainBrandColor} !important;
+    border: 1px solid ${props => props.theme.mainBrandColor} !important;
   }
 `;
 
@@ -25,9 +35,11 @@ const Shop = ({ item }) => {
             <p className="has-text-weight-semibold has-text-info">
               {item.node.title}
             </p>
-            <Button width="14rem" background="transparent">
-              ENQUIRE
-            </Button>
+            <a href="mailto:enquiries@deckerdence.com">
+              <Button type="button" className="button is-large is-radiusless ">
+                ENQUIRE
+              </Button>
+            </a>
           </div>
         </div>
       </div>
